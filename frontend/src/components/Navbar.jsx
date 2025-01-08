18fdcbb245eae2c8c4205df27173e2ff78c0ffca
usriram <imsriram10@gmail.com>
2025-01-08 23:20:48 +0530

    Made transparent navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,35 +5,45 @@ import { Link, useNavigate } from "react-router-dom";
 const Navbar = () => {
   const navigate=useNavigate()
   return (
-
-    <AppBar position="static" sx={{
-      background: "#4caf50"
-
-
-    }}>
-      <Toolbar sx={{  }}>
-        <Typography variant="h6" sx={{
-          flexGrow: 1,
-          cursor:"pointer"
+    <AppBar
+      position="fixed"
+      elevation={0} // Remove shadow
+      sx={{
+        background: "rgba(150, 255, 150, 0.3)", // Transparent white
+        backdropFilter: "blur(10px)", // Frosted glass effect
+        boxShadow: "3px 4px 30px rgba(0, 0, 0, 0.2)", // Optional soft shadow
+        // borderBottom: "1px solid rgba(255, 255, 255, 0.2)", // Optional border
+        zIndex: 1301, // Ensures it overlaps the content
+      }}
+    >
+      <Toolbar
+        sx={{
+          color: "#323232",
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            flexGrow: 1,
+            cursor: "pointer",
           }}
-          onClick={()=> navigate("/")}
-          >
+          onClick={() => navigate("/")}
+        >
           IoT & ML-Based CRFOS
         </Typography>
 
-        <Button color="inherit" component={Link} to="/home">
+        <Button component={Link} to="/home">
           Home
         </Button>
-        <Button color="inherit" component={Link} to="/login">
+        <Button component={Link} to="/login">
           Log in
         </Button>
-        <Button color="inherit" component={Link} to="/register">
+        <Button component={Link} to="/register">
           Register
         </Button>
-        <Button color="inherit" component={Link} to="/About">
+        <Button component={Link} to="/About">
           About us
         </Button>
-
       </Toolbar>
     </AppBar>
     // <Box sx={{
